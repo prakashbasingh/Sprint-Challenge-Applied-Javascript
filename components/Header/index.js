715bb36b53bda6385ed078d03b0ca101b51a8dbe
unknown <prakashbahadursingh2@gmail.com>
2020-04-10 08:31:25 -0700

    elements created, nested, class name given

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,20 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+// creating creating
+function Header() {
+    const headerDiv = document.createElement('div')
+    const spanFirst = document.createElement('span')
+    const headerH1 = document.createElement('h1')
+    const spanSecond = document.createElement('span')
+}
+// element nesting
+headerDiv.appendChild(spanFirst)
+headerDiv.appendChild(headerH1)
+headerDiv.appendChild(spanSecond)
+
+// adding class name
+headerDiv.classList.add('header')
+spamFirst.classList.add('date')
+spanSecond.classList.add('temp')
+
